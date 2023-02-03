20374334c5951a7bc82dd2d7a914e71910865cfc
unknown <mahed.chowdhury@iboxhealthcare.co.uk>
2023-02-03 18:35:14 +0600

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,46 @@
+<div class="footer pt-5 pb-5 mt-5">
+    <div class="row row-cols-lg-4">
+        <div class="d-flex justify-content-center">
+            <div>
+                <h5>Home</h5>
+                <h5>About</h5>
+                <h5>Contact</h5>
+                <h5>Dashboard</h5>
+                <h5>Gallery</h5>
+            </div>
+
+        </div>
+        <div class="d-flex justify-content-center">
+            <div>
+                <h5>Home</h5>
+                <h5>About</h5>
+                <h5>Contact</h5>
+                <h5>Dashboard</h5>
+                <h5>Gallery</h5>
+            </div>
+
+        </div>
+        <div class="d-flex justify-content-center">
+            <div>
+                <h5>Home</h5>
+                <h5>About</h5>
+                <h5>Contact</h5>
+                <h5>Dashboard</h5>
+                <h5>Gallery</h5>
+            </div>
+
+        </div>
+        <div class="d-flex justify-content-center">
+            <div>
+                <h5>Home</h5>
+                <h5>About</h5>
+                <h5>Contact</h5>
+                <h5>Dashboard</h5>
+                <h5>Gallery</h5>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
